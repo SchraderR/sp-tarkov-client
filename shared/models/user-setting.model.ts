bb2afecb06c28baf130b8be28a4d98559e56c276
Philipp Heenemann <heenemann.p@hotmail.de>
2023-07-11 23:17:53 +0200

    Add UserSetting model for handling user configurations
    This commit introduces a new model `UserSetting` that allows to store specific user configurations such as associated Aki instances. This is a necessary addition as it efficiently assists users to select and manage different Aki instances within the application.

---
+++ shared/models/user-setting.model.ts
@@ -0,0 +1,13 @@
+﻿import { AkiCore } from './aki-core.model';
+
+export interface UserSettingStoreModel {
+  akiInstances: AkiInstance[];
+}
+
+export interface AkiInstance {
+  akiRootDirectory: string;
+}
+
+export interface UserSettingModel extends AkiInstance {
+  akiCore: AkiCore;
+}
