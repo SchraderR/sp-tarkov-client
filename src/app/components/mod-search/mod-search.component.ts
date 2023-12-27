c3cfb765504c8ca1cfc02e88854a612c5d98fdd6
Philipp Heenemann <heenemann.p@hotmail.de>
2023-12-27 21:10:01 +0100

    Update mod-search component
    The mod-search component's dependencies have been adjusted to include 'NgForOf'. Styling changes in the mod-search.component.scss have been implemented to improve UI consistency. The HTML template also saw some modifications to improve user interactivity and display behavior.

--- src/app/components/mod-search/mod-search.component.ts
+++ src/app/components/mod-search/mod-search.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, ViewEncapsulation } from '@angular/core';
-import { AsyncPipe, NgIf, NgOptimizedImage } from '@angular/common';
+import { AsyncPipe , NgForOf , NgIf , NgOptimizedImage } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -25,6 +25,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatIconModule,
     NgIf,
+    NgForOf,
   ],
   templateUrl: './mod-search.component.html',
   styleUrl: './mod-search.component.scss',
