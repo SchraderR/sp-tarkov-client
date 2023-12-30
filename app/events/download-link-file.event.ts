61fd1835c781cbbca3e42cdcb261a1c068a5e3c2
Philipp Heenemann <heenemann.p@hotmail.de>
2023-12-30 16:27:56 +0100

    Add functionality for downloading GitHub releases
    Updated download-link-file.event.ts to include new functionality for handling and downloading GitHub release archives. This involves the parsing of GitHub links to obtain necessary download data and axios is added to manage the HTTP requests. Changes include the addition of helper functions, new custom data structures, and the download handler. Added axios to package.json, and new data structures are defined in github.model.ts.

--- app/events/download-link-file.event.ts
+++ app/events/download-link-file.event.ts
@@ -1,9 +1,17 @@
 ﻿import { ipcMain } from 'electron';
 import { Browser, launch, Page } from 'puppeteer';
+import axios from 'axios';
+import { GithubRelease } from '../../shared/models/github.model';
 
 export interface EventFileArgs<T = any> {
   fileId: string;
   parameter: T;
+}
+
+export interface GithubLinkData {
+  userName: string;
+  repoName: string;
+  tag: string;
 }
 
 export const handleDownloadLinkEvent = () => {
@@ -38,8 +46,32 @@ export const handleDownloadLinkEvent = () => {
       const newPage: Page = await newPagePromise;
 
       downloadLink = await newPage.$eval('a[href]', e => e.getAttribute('href'));
-      event.sender.send('download-link-completed', downloadLink);
+      if (!downloadLink) {
+        await browser.close();
+        return;
+      }
 
+      const isArchiveLink = isArchiveURL(downloadLink);
+      if (!isArchiveLink) {
+        const gitHubInformation = parseGitHubLink(downloadLink);
+        console.log(gitHubInformation);
+        if (!gitHubInformation) {
+          //  TODO Error Handling
+          // await browser.close();
+          return;
+        }
+        await getReleaseData(gitHubInformation)
+          .then(async data => {
+            const githubDownloadLink = data?.assets?.[0].browser_download_url;
+            console.log(githubDownloadLink);
+            event.sender.send('download-link-completed', githubDownloadLink);
+            await browser.close();
+            return;
+          })
+          .catch(err => console.error(err));
+      }
+
+      event.sender.send('download-link-completed', downloadLink);
       await browser.close();
     })();
   });
@@ -57,3 +89,47 @@ const getNewPageWhenLoaded = async (browser: Browser) => {
     })
   );
 };
+
+function isArchiveURL(url: string): boolean {
+  const extensions = ['zip', 'rar', '7z', 'tar', 'gz'];
+
+  const urlSegments = url.split('/');
+  const fileName = urlSegments[urlSegments.length - 1];
+
+  const fileSegments = fileName.split('.');
+  const fileExtension = fileSegments[fileSegments.length - 1];
+
+  return extensions.includes(fileExtension);
+}
+
+function parseGitHubLink(url: string): GithubLinkData | null {
+  const regex = /https:\/\/github\.com\/(.*?)\/(.*?)\/releases\/tag\/(.*)/;
+  const matches = url.match(regex);
+
+  if (matches && matches.length === 4) {
+    return {
+      userName: matches[1],
+      repoName: matches[2],
+      tag: matches[3],
+    };
+  } else {
+    return null;
+  }
+}
+
+async function getReleaseData({ tag, userName, repoName }: GithubLinkData) {
+  const url = `https://api.github.com/repos/${userName}/${repoName}/releases/tags/${tag}`;
+
+  try {
+    const response = await axios.get<GithubRelease>(url);
+    console.log('X-RateLimit-Remaining:', response.headers['x-ratelimit-remaining']);
+    console.log('X-RateLimit-Reset:', response.headers['x-ratelimit-reset']);
+    console.log('X-RateLimit-Limit:', response.headers['x-ratelimit-limit']);
+    console.log('X-RateLimit-Used:', response.headers['x-ratelimit-used']);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
