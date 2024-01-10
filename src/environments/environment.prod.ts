a34f878a0326069ff6ca7df2e20b87e93c9c995e
Philipp Heenemann <heenemann.p@hotmail.de>
2024-01-10 17:01:51 +0100

    Update akiHubLink in environment configurations
    This update adds the correct akiHubLink in both the development and production environment configurations. The unneeded import of zone-error from zone.js in the development environment file has also been removed.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const APP_CONFIG = {
   production: true,
   environment: 'PROD',
   githubIssueUrl: 'https://github.com/SchraderR/sp-tarkov-client/issues',
-  akiHubLink: '',
+  akiHubLink: 'https://hub.sp-tarkov.com/files/file/1702-sp-eft-manager/',
 };
