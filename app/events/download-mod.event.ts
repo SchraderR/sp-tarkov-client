778ffccd300affd7fa4976f0b89fb117e4d73b40
Philipp Heenemann <heenemann.p@hotmail.de>
2024-01-23 09:29:19 +0100

    Add download capability for Google Docs links
    The code changes introduce the functionality to handle download requests for mod files hosted on Google Docs. The download process is performed by launching a headless browser using puppeteer, which navigates to the provided URL, waits for the network to calm, and downloads the file. This functionality is essential for situations where the mod files are hosted on Google Docs.

--- app/events/download-mod.event.ts
+++ app/events/download-mod.event.ts
@@ -1,16 +1,30 @@
-﻿import { ipcMain } from 'electron';
+﻿import { app, ipcMain } from 'electron';
 import { download } from 'electron-dl';
 import { BrowserWindowSingleton } from '../browserWindow';
 import { DownloadModel } from '../../shared/models/aki-core.model';
 import * as path from 'path';
 import * as fs from 'fs';
 import axios from 'axios';
+import { Browser, launch } from 'puppeteer';
 
 export const handleDownloadModEvent = () => {
   ipcMain.on('download-mod', async (event, downloadModel: DownloadModel) => {
+    let browser: Browser | null = null;
     const ankiTempDownloadDir = path.join(downloadModel.akiInstancePath, '_temp');
     if (!fs.existsSync(ankiTempDownloadDir)) {
       fs.mkdirSync(ankiTempDownloadDir);
+    }
+
+    if (downloadModel.modFileUrl.includes('docs.google.com')) {
+      browser = await launch({
+        headless: 'new',
+        executablePath: `${app.getPath('home')}/.local-chromium/chrome/win64-122.0.6257.0/chrome-win64/chrome.exe`,
+      });
+
+      const isDownloadCompleted = await waitForDownload(downloadModel, browser, event, ankiTempDownloadDir);
+      if (isDownloadCompleted) {
+        return;
+      }
     }
 
     if (downloadModel.modFileUrl.startsWith('https://hub.sp-tarkov.com/files/download')) {
@@ -32,4 +46,42 @@ async function handleDirectDownload(event: Electron.IpcMainEvent, ankiTempDownlo
   await axios({ url: downloadModel.modFileUrl, method: 'GET', responseType: 'stream' });
 
   file.on('finish', () => event.sender.send('download-mod-completed', file));
+}
+
+async function waitForDownload(downloadModel: DownloadModel, browser: Browser, event: Electron.IpcMainEvent, ankiTempDownloadDir: string) {
+  return new Promise<boolean>(async (resolve, reject) => {
+    try {
+      let guids: any = {};
+
+      const page = await browser.newPage();
+      const client = await page.target().createCDPSession();
+      await client.send('Browser.setDownloadBehavior', { behavior: 'allowAndName', downloadPath: ankiTempDownloadDir, eventsEnabled: true });
+
+      client.on('Browser.downloadWillBegin', async event => (guids[event.guid] = event.suggestedFilename));
+      client.on('Browser.downloadProgress', async progressEvent => {
+        if (progressEvent.state === 'inProgress') {
+          event.sender.send('download-mod-progress', {
+            percent: progressEvent.receivedBytes / progressEvent.totalBytes,
+            transferredBytes: progressEvent.receivedBytes,
+            totalBytes: progressEvent.totalBytes,
+          });
+        } else if (progressEvent.state === 'completed') {
+          fs.renameSync(path.resolve(ankiTempDownloadDir, progressEvent.guid), path.resolve(ankiTempDownloadDir, guids[progressEvent.guid]));
+          event.sender.send('download-mod-progress', {
+            percent: 100,
+            transferredBytes: progressEvent.receivedBytes,
+            totalBytes: progressEvent.totalBytes,
+          });
+          event.sender.send('download-mod-completed', path.resolve(ankiTempDownloadDir, guids[progressEvent.guid]));
+          await browser?.close();
+          resolve(true); // Resolve promise here
+        }
+      });
+
+      await page.goto(downloadModel.modFileUrl, { waitUntil: 'networkidle2' });
+      await browser.close();
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
