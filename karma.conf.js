617a5e02e0b4b81c26b6a0c134a2678db1b29b94
Philipp Heenemann <heenemann.p@hotmail.de>
2024-02-12 21:20:13 +0100

    Refactor karma.conf.js and update angular-main.yml
    Relocated the configuration file karma.conf.js from the 'src' folder to the root directory. Additionally, updated the GitHub actions workflow file, angular-main.yml, to set up Google Chrome. This change in the location of the karma.conf.js should aid better visibility and ease of access, while the update to the workflow ensures the required browser setup for running tests.

--- karma.conf.js
+++ karma.conf.js
@@ -15,10 +15,10 @@ module.exports = function (config) {
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true,
+    coverageReporter: {
+      dir: require('path').join(__dirname, 'coverage/'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
