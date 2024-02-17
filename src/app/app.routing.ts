2a8aa85e55b1825a2f31cbf93ba1435a5fdb9392
Philipp Heenemann <heenemann.p@hotmail.de>
2024-02-17 10:35:25 +0100

    Refactor workflow and routing, update personal setting UI
    Simplify the syntax in the GitHub workflow and Angular routing files, making them more concise. Update the styling of the 'Personal Setting' component and add a new 'active-star' class. This makes the codebase easier to maintain and improves the UI of the 'Personal Setting' component.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,49 +1,27 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: 'setting',
-    pathMatch: 'full',
-  },
-  {
-    path: 'setting',
-    loadComponent: () => import('./components/personal-setting/personal-setting.component'),
-  },
+  { path: '', redirectTo: 'setting', pathMatch: 'full' },
+  { path: 'setting', loadComponent: () => import('./components/personal-setting/personal-setting.component') },
   {
     path: 'top-rated',
     loadComponent: () => import('./core/components/generic-mod-list/generic-mod-list.component'),
-    data: {
-      sortField: 'cumulativeLikes',
-      sortOrder: 'DESC',
-    },
+    data: { sortField: 'cumulativeLikes', sortOrder: 'DESC' },
   },
   {
     path: 'new',
     loadComponent: () => import('./core/components/generic-mod-list/generic-mod-list.component'),
-    data: {
-      sortField: 'time',
-      sortOrder: 'DESC',
-    },
+    data: { sortField: 'time', sortOrder: 'DESC' },
   },
   {
     path: 'updated',
     loadComponent: () => import('./core/components/generic-mod-list/generic-mod-list.component'),
-    data: {
-      sortField: 'lastChangeTime',
-      sortOrder: 'DESC',
-    },
+    data: { sortField: 'lastChangeTime', sortOrder: 'DESC' },
   },
   {
     path: 'most-loaded',
     loadComponent: () => import('./core/components/generic-mod-list/generic-mod-list.component'),
-    data: {
-      sortField: 'downloads',
-      sortOrder: 'DESC',
-    },
+    data: { sortField: 'downloads', sortOrder: 'DESC' },
   },
-  {
-    path: 'mod-list',
-    loadComponent: () => import('./components/mod-list/mod-list.component'),
-  },
+  { path: 'mod-list', loadComponent: () => import('./components/mod-list/mod-list.component') },
 ];
