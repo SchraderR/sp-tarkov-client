4b888c548509dc4507a6ee044189aec20a57d216
Philipp Heenemann <heenemann.p@hotmail.de>
2024-03-08 20:27:35 +0100

    Remove unused import in TarkovStartComponent spec
    The commit removes the import of 'applicationTarkovInstanceOutputEventNames' from 'electron.events', and 'of' from 'rxjs' in tarkov-start.component.spec.ts file, as it was not being used anywhere in the component. Also, imports are organized for better readability.

--- src/app/components/tarkov-start/tarkov-start.component.spec.ts
+++ src/app/components/tarkov-start/tarkov-start.component.spec.ts
@@ -1,10 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TarkovStartComponent } from './tarkov-start.component';
 import { mockProvider } from '@ngneat/spectator';
 import { ElectronService } from '../../core/services/electron.service';
-import { applicationTarkovInstanceOutputEventNames } from '../../core/events/electron.events';
-import { EMPTY, of } from 'rxjs';
+import { EMPTY } from 'rxjs';
 
 describe('TarkovStartComponent', () => {
   let component: TarkovStartComponent;
