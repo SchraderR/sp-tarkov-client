b98afad5c5cc370d8e5a349b94f5a07c6d411afb
Philipp Heenemann <heenemann.p@hotmail.de>
2024-04-02 08:22:15 +0200

    Add mockProvider to instance-overview test setup
    The instance-overview component test setup has been made robust by including mockProvider. This change allows the ElectronService to be mocked, ensuring that the tests are not dependent on the actual service but mimic its behavior using a mockProvider. This enhances the reliability of the tests.

--- src/app/components/instance-overview/instance-overview.component.spec.ts
+++ src/app/components/instance-overview/instance-overview.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import InstanceOverviewComponent from './instance-overview.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { mockProvider } from '@ngneat/spectator';
+import { ElectronService } from '../../core/services/electron.service';
+import { EMPTY } from 'rxjs';
 
 describe('InstanceOverviewComponent', () => {
   let component: InstanceOverviewComponent;
@@ -9,6 +12,11 @@ describe('InstanceOverviewComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [InstanceOverviewComponent, RouterTestingModule],
+      providers: [
+        mockProvider(ElectronService, {
+          sendEvent: () => EMPTY,
+        }),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InstanceOverviewComponent);
