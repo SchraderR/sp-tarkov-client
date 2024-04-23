d4661803e01b7c66cfd8cdeb17e14204a917a9f4
Philipp Heenemann <heenemann.p@hotmail.de>
2024-04-23 23:20:45 +0200

    Add new items to restrictedModList
    The restrictedModList constant has been updated to include 'SPT Mini-Launcher', 'Quick Launcher', 'Unity Assets Bundle Extractor Avalonia', and 'DebugPlus'. These additions ensure that these mods are also taken into account in the restriction process.

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,12 @@
-﻿export const restrictedModList = ['SP-EFT Manager', 'SPT-AKI', 'SPT-AKI-INSTALLER', 'AKI Patcher', 'SPT-Aki Profile Editor', 'AKI Mods Installer'];
+﻿export const restrictedModList = [
+  'SP-EFT Manager',
+  'SPT-AKI',
+  'SPT-AKI-INSTALLER',
+  'SPT Mini-Launcher',
+  'AKI Patcher',
+  'SPT-Aki Profile Editor',
+  'AKI Mods Installer',
+  'Quick Launcher',
+  'Unity Assets Bundle Extractor Avalonia',
+  'DebugPlus',
+];
