583a2542ac2dffc11018ce86687881124e7b3366
Philipp Heenemann <heenemann.p@hotmail.de>
2024-05-08 15:33:43 +0200

    Update tests setup and improve logs consistency
    This commit updates the setup of various tests and replaces string formatting in logs for consistency. It also introduces new providers into the unit tests for 'AppComponent' and 'ModListComponent'. Lastly, it improves the download services by ensuring logs use single quotes for consistency.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,31 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { mockProvider } from '@ngneat/spectator';
 import { ElectronService } from './core/services/electron.service';
 import { TranslocoModule } from '@jsverse/transloco';
-import { RouterTestingModule } from '@angular/router/testing';
+import { JoyrideModule } from 'ngx-joyride';
+import { of } from 'rxjs';
+import { provideRouter } from '@angular/router';
 
-xdescribe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
+describe('AppComponent', () => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule, AppComponent, RouterTestingModule, TranslocoModule, HttpClientTestingModule],
-      providers: [mockProvider(ElectronService)],
+      imports: [NoopAnimationsModule, AppComponent, JoyrideModule.forRoot(), TranslocoModule, HttpClientTestingModule],
+      providers: [
+        provideRouter([]),
+        mockProvider(ElectronService, {
+          sendEvent: () => of(),
+          getGithubRateLimitInformation: () => of(),
+        }),
+      ],
     }).compileComponents();
-  }));
+  });
 
-  it('should create the app', waitForAsync(() => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
+  });
 });
