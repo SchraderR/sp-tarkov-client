55e306cf631669d419920bb060bd465c69b19afc
Philipp Heenemann <philipp.heenemann@outlook.de>
2024-08-17 11:57:09 +0200

    Add mod load order warning dialog
    Introduce a user warning dialog advising against changing the mod load order without instruction. Implement dialog open and close functionality within the component initialization.

--- src/app/components/mod-load-order/mod-load-order.component.ts
+++ src/app/components/mod-load-order/mod-load-order.component.ts
@@ -1,9 +1,12 @@
-import { Component, DestroyRef, inject, NgZone, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, NgZone, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ElectronService } from '../../core/services/electron.service';
 import { CommonModule } from '@angular/common';
 import { UserSettingsService } from '../../core/services/user-settings.service';
 import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 export interface ModLoadOrder {
   order: string[];
@@ -14,17 +17,23 @@ export interface ModLoadOrder {
   selector: 'app-mod-load-order',
   templateUrl: './mod-load-order.component.html',
   styleUrl: './mod-load-order.component.scss',
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, DragDropModule, MatButton, MatCard, MatCardActions, MatCardContent],
 })
 export default class ModLoadOrderComponent implements OnInit {
+  @ViewChild('modLoadOrderWarning', { static: true }) modLoadOrderWarning!: TemplateRef<unknown>;
+
   #electronService = inject(ElectronService);
   #userSettingsService = inject(UserSettingsService);
   #ngZone = inject(NgZone);
   #destroyRef = inject(DestroyRef);
+  #matDialog = inject(MatDialog);
 
+  modLoadOrderWarningDialog!: MatDialogRef<unknown, unknown>;
   modLoadOrder: ModLoadOrder = { order: [] };
 
   ngOnInit() {
+    this.openModLoadOrderDialog();
+
     const instancePath =
       this.#userSettingsService.getActiveInstance()?.sptRootDirectory ?? this.#userSettingsService.getActiveInstance()?.akiRootDirectory;
     if (!instancePath) {
@@ -52,5 +61,16 @@ export default class ModLoadOrderComponent implements OnInit {
         modLoadOrder: this.modLoadOrder.order,
       })
       .subscribe();
+  }
+
+  closeModLoadOrderDialog() {
+    this.modLoadOrderWarningDialog.close();
+  }
+
+  private openModLoadOrderDialog() {
+    this.modLoadOrderWarningDialog = this.#matDialog.open(this.modLoadOrderWarning, {
+      disableClose: true,
+      width: '50%',
+    });
   }
 }
