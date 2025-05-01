718286be15b138bea6832e5abc388ee00c242d47
Philipp Heenemann <philipp.heenemann@outlook.de>
2025-05-01 11:17:37 +0200

    Update Angular 18

--- src/app/core/services/spt-search.service.spec.ts
+++ src/app/core/services/spt-search.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { SptSearchService } from './spt-search.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 describe('SptSearchService', () => {
   let service: SptSearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    TestBed.configureTestingModule({ imports: [], providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()] });
     service = TestBed.inject(SptSearchService);
   });
 
