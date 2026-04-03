495fbb1368734e05f67487c01d49cac1e97e0f5d
Schrader <argetlamcrank@gmail.com>
2026-04-03 21:32:15 +0200

    temporary reactivate remote config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   githubIssueUrl: 'https://github.com/SchraderR/sp-tarkov-client/issues',
   sptHubEftManagerLink: 'https://hub.sp-tarkov.com/files/file/1702-sp-eft-manager/',
   githubConfigLink: 'https://schraderr.github.io/sp-tarkov-client-configuration',
-  ignoreRemoteConfig: true,
+  ignoreRemoteConfig: false,
 };
